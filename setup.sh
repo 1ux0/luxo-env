0e5071cced4f9fa7816327aa7fa8c9265baaf8a3
1ux0 <luxo.ai@proton.me>
2026-03-26 12:10:12 -0400

    update name

--- setup.sh
+++ setup.sh
@@ -10,61 +10,61 @@ NO_JS=false
 NO_PYTHON=false
 
 show_help() {
-  echo "Usage: $0 [OPTIONS]"
-  echo ""
-  echo "Options:"
-  echo "  --no-vim      Skip Vim setup."
-  echo "  --no-bin      Skip Binary setup."
-  echo "  --no-haskell  Skip Haskell setup."
-  echo "  --no-js       Skip JavaScript setup."
-  echo "  --no-python   Skip Python setup."
-  echo "  -h, --help    Show this help message and exit."
-  exit 0
+	echo "Usage: $0 [OPTIONS]"
+	echo ""
+	echo "Options:"
+	echo "  --no-vim      Skip Vim setup."
+	echo "  --no-bin      Skip Binary setup."
+	echo "  --no-haskell  Skip Haskell setup."
+	echo "  --no-js       Skip JavaScript setup."
+	echo "  --no-python   Skip Python setup."
+	echo "  -h, --help    Show this help message and exit."
+	exit 0
 }
 
 while [[ "$#" -gt 0 ]]; do
-  case "$1" in
-    --no-vim) NO_VIM=true ;;
-    --no-bin) NO_BIN=true ;;
-	--no-haskell) NO_HASKELL=true ;;
-	--no-js) NO_JS=true ;;
-	--no-python) NO_PYTHON=true ;;
-    -h|--help) show_help ;;
-    --) shift; break ;; # End of options
-    *) echo "Unknown option: $1" >&2; exit 1 ;;
-  esac
-  shift
+	case "$1" in
+		--no-vim) NO_VIM=true ;;
+		--no-bin) NO_BIN=true ;;
+		--no-haskell) NO_HASKELL=true ;;
+		--no-js) NO_JS=true ;;
+		--no-python) NO_PYTHON=true ;;
+		-h|--help) show_help ;;
+		--) shift; break ;; # End of options
+		*) echo "Unknown option: $1" >&2; exit 1 ;;
+	esac
+	shift
 done
 
 echo "
-  _                 
- | |_   ___  _____  
- | | | | \ \/ / _ \ 
- | | |_| |>  < (_) |
- |_|\__,_/_/\_\___/  .ai                  
-"
+_                 
+| |_   ___  _____  
+| | | | \ \/ / _ \ 
+	| | |_| |>  < (_) |
+	|_|\__,_/_/\_\___/  .env                  
+	"
 
-if ! command -v brew >/dev/null 2>&1; 
-then 
-	print_yellow "Installing Homebrew..."
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"	
-fi
+	if ! command -v brew >/dev/null 2>&1; 
+	then 
+		print_yellow "Installing Homebrew..."
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"	
+	fi
 
-print_blue "Updating Homebrew..."
-brew update
-sleep 3
+	print_blue "Updating Homebrew..."
+	brew update
+	sleep 3
 
-print_blue "Installing Homebrew base packages..."
+	print_blue "Installing Homebrew base packages..."
 
-brew install gnupg \
-	 pinentry-mac \
-	 httpie
+	brew install gnupg \
+		pinentry-mac \
+		httpie
 
-print_green "Done"
-sleep 3
+	print_green "Done"
+	sleep 3
 
-./git-setup.sh
-sleep 3
+	./git-setup.sh
+	sleep 3
 
 #./zsh-setup.sh
 sleep 3
